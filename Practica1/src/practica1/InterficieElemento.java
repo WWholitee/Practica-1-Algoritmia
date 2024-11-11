7ac41623377a0ad5f1d77b0fd8b962a6680391db
elenamorey6 <elenamorey23@gmail.com>
2024-11-11 16:57:17 +0100

    InterficieElemento.java

---
+++ Practica1/src/practica1/InterficieElemento.java
@@ -0,0 +1,12 @@
+package practica1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface InterficieElemento {
+
+    //Métodos para obtener el identificador y la descripción de un elemento
+    String getIdentificador();
+
+    String getDescripcion();
+}
