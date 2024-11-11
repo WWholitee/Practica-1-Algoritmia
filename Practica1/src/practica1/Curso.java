166a4ff9c25486877ee030e0c72c8ff8e1151283
elenamorey6 <elenamorey23@gmail.com>
2024-11-11 16:44:57 +0100

    Curso.java

--- Practica1/src/practica1/Curso.java
+++ Practica1/src/practica1/Curso.java
@@ -1,26 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
 package practica1;
 
-/**
- *
- * @author ellen
- */
-public class Curso {
+import java.util.ArrayList;
+import java.util.List;
 
-   private String nombre_c;
-   private String codi_c;
-   
-   public Curso(String nombre, String codigo){
-       this.codi_c=codigo;
-       this.nombre_c=nombre;
-         
-       }
-   
-   public String toString(){
-       return "Curso: "+ nombre_c+"Con codigo:"+ codi_c;
-   }
-   }
+public abstract class Curso {
+    // Atributos
+    private String nombre_c;
+    private String codi_c;
+    
+    // Lista de asignaturas asociadas al curso
+    private List<Assignatura> asignaturas = new ArrayList<>();
 
+    // Constructor
+    public Curso(String nombre, String codigo) {
+        this.codi_c = codigo;
+        this.nombre_c = nombre;
+    }
+
+    // Métodos Getters
+    public String getNombre() {
+        return nombre_c;
+    }
+
+    public String getCodigo() {
+        return codi_c;
+    }
+
+    // Métodos para agregar y eliminar asignaturas
+    public void agregarAsignatura(Assignatura asignatura) {
+        asignaturas.add(asignatura);
+    }
+
+    public void eliminarAsignatura(Assignatura asignatura) {
+        asignaturas.remove(asignatura);
+    }
+
+    // Método para listar todas las asignaturas
+    public List<Assignatura> listarAsignaturas() {
+        return asignaturas;
+    }
+
+    public abstract String getTipo();
+
+    public abstract void mostrarDatos();
+
+    // Método toString
+    @Override
+    public String toString() {
+        return "Curso: " + nombre_c + " con codigo: " + codi_c;
+    }
+}
