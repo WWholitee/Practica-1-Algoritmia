bbfb8d9de326be70329158499141289aae43263d
elenamorey6 <elenamorey23@gmail.com>
2024-11-18 22:20:51 +0100

    Update Practica1.java

--- Practica1/src/practica1/Practica1.java
+++ Practica1/src/practica1/Practica1.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package practica1;
 
 import java.util.Scanner;
@@ -12,68 +8,101 @@ import java.util.Scanner;
  */
 public class Practica1 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num;
+        LlistaCurs llistaCursos = new LlistaCurs();  // Lista de cursos
+        ListaEstudiantes listaEstudiantes = new ListaEstudiantes();  // Lista de estudiantes
+
         do {
-            System.out.println("OPCION 1:Dar de Alta un Curso y sus Asignaturas");
-            System.out.println("OPCION 2:Dar de Alta un alumno dada una asignatura a la que se quiere matricular");
+            System.out.println("OPCION 1: Dar de Alta un Curso y sus Asignaturas");
+            System.out.println("OPCION 2: Dar de Alta un alumno dada una asignatura a la que se quiere matricular");
+            System.out.println("OPCION 3: Dar de baja un curso");
             num = scanner.nextInt();
+            scanner.nextLine(); 
+
             switch (num) {
                 case 1:
+                    // Dar de alta un curso y sus asignaturas
                     Curso curs;
                     System.out.println("Bachiller(1) o FP(2)");
                     int tipo = scanner.nextInt();
                     scanner.nextLine();
                     if (tipo == 1) {
-                        System.out.println("Escriba el nombre, el codigo y el curso del Bachiller que quiere añadir");
-                        curs = new Batxiller(scanner.next(), scanner.next(), scanner.next());
+                        // Crear un curso de Bachillerato
+                        System.out.println("Escriba el nombre, el código y el curso del Bachiller que quiere añadir");
+                        curs = new Batxiller(scanner.nextLine(), scanner.nextLine(), scanner.nextLine());
                     } else {
-                        System.out.println("Escriba el nombre, el codigo y el curso de la Formación Profesional que quiere añadir");
-                        curs = new FormacioProfessional(scanner.next(), scanner.next(), scanner.next());
+                        // Crear un curso de Formación Profesional
+                        System.out.println("Escriba el nombre, el código y la especialidad de la Formación Profesional que quiere añadir");
+                        curs = new FormacioProfessional(scanner.nextLine(), scanner.nextLine(), scanner.nextLine());
                     }
 
-                    System.out.println("Afegir assignatures");
-                    System.out.println("Dime cuantas asignaturas quieres meter");
+                    System.out.println("Añadir asignaturas");
+                    System.out.println("Dime cuántas asignaturas quieres añadir");
                     int numa = scanner.nextInt();
+                    scanner.nextLine(); 
                     for (int i = 0; i < numa; i++) {
                         Assignatura a;
-                        System.out.println("Añade nombre, codigo y numero de credits");
-                        a = new AssignaturaOblig(scanner.next(), scanner.next(), scanner.nextInt());
+                        System.out.println("Añade nombre, código y número de créditos");
+                        a = new AssignaturaOblig(scanner.nextLine(), scanner.nextLine(), scanner.nextInt());
+                        scanner.nextLine();
                         curs.AddAssignatura(a);
                     }
-                    System.out.println("Lista assignaturas");
+                    System.out.println("Lista de asignaturas:");
                     System.out.println(curs.StringAssignaturas());
+                    llistaCursos.addObject(curs);
                     break;
+
                 case 2:
+                    // Dar de alta un estudiante y matricularlo en una asignatura
                     Assignatura assignatura;
                     Estudiantes estudiante;
-                    ListaEstudiantes listaE = new ListaEstudiantes();
                     System.out.println("Es una asignatura obligatoria(1) o opcional(2)");
                     int aux = scanner.nextInt();
                     scanner.nextLine();
                     if (aux == 1) {
-                        System.out.println("Cual es la asignatura obligatoria a la que te quieres matricular? (Escribe el nombre, el codigo y los créditos");
+                        System.out.println("¿Cuál es la asignatura obligatoria a la que te quieres matricular? (Escribe el nombre, el código y los créditos)");
                         assignatura = new AssignaturaOblig(scanner.nextLine(), scanner.nextLine(), scanner.nextInt());
-                        scanner.nextLine();
+                        scanner.nextLine(); 
                     } else {
-                        System.out.println("Cual es la asignatura opcional a la que te quieres matricular? (Escribe el nombre, el codigo y el perfil");
+                        System.out.println("¿Cuál es la asignatura opcional a la que te quieres matricular? (Escribe el nombre, el código y el perfil)");
                         assignatura = new AssignaturaOpt(scanner.nextLine(), scanner.nextLine(), scanner.nextLine());
                     }
                     System.out.println("Escribe el nombre y el DNI del estudiante");
                     estudiante = new Estudiantes(scanner.nextLine(), scanner.nextLine());
                     estudiante.matricularAsignatura(assignatura);
-                    listaE.addEstudiante(estudiante);
-                    System.out.println("Estudiante " + estudiante.getNombre() + " con DNI " + estudiante.getDNI() + " matriculado a la asignatura " + assignatura.toString());
+                    listaEstudiantes.addEstudiante(estudiante);
+                    System.out.println("Estudiante " + estudiante.getNombre() + " con DNI " + estudiante.getDNI() + " matriculado en la asignatura " + assignatura.toString());
                     break;
+
+                case 3:
+                    // Dar de baja un curso y sus asignaturas
+                    System.out.println("Introduce el nombre del curso a dar de baja:");
+                    String cursoNombre = scanner.nextLine();
+                    boolean encontrado = false;
+                    for (int i = 0; i < llistaCursos.longitud(); i++) {
+                        Curso curso = (Curso) llistaCursos.lista().get(i);
+                        if (curso.getNombre().equals(cursoNombre)) {
+                            // El curso fue encontrado
+                            llistaCursos.lista().remove(i);
+                            encontrado = true;
+                            System.out.println("Curso '" + cursoNombre + "' dado de baja correctamente.");
+                            break;
+                        }
+                    }
+                    if (!encontrado) {
+                        System.out.println("Curso no encontrado.");
+                    }
+                    break;
+
                 default:
-                    System.out.println("no es una opcio valida");
+                    System.out.println("Opción no válida.");
                     break;
             }
         } while (num != 0);
+
+        scanner.close();  // Cerrar el scanner
     }
-    
 }
+
