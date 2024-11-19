15c349c683aa0e3e281c2e89152cae9c504f0580
elenamorey6 <elenamorey23@gmail.com>
2024-11-19 22:34:14 +0100

    no imprime estudiantes LlistaAssignatura.java

--- Practica1/src/practica1/LlistaAssignatura.java
+++ Practica1/src/practica1/LlistaAssignatura.java
@@ -7,24 +7,27 @@ import java.util.ArrayList;
  * @author ellen
  */
 public class LlistaAssignatura implements InterficieLista {
+
     private ArrayList<Assignatura> LlistaAssignatura;
-    public LlistaAssignatura(){
-        this.LlistaAssignatura= new ArrayList<>();
+
+    public LlistaAssignatura() {
+        this.LlistaAssignatura = new ArrayList<>();
     }
-    
-    public void imprimirAssignatura(){
-        for(Assignatura a : LlistaAssignatura){
-            a.imprimirMatriculados();
+
+    public void imprimirAssignatura() {
+        for (Assignatura asignatura : LlistaAssignatura) {
+            asignatura.imprimirMatriculados();
         }
     }
+
     @Override
     public int longitud() {
         return LlistaAssignatura.size();
     }
 
     @Override
-    public void addObject(Object a) {
-        this.LlistaAssignatura.add((Assignatura) a);
+    public void addObject(Object asignatura) {
+        this.LlistaAssignatura.add((Assignatura) asignatura);
     }
 
     @Override
@@ -36,18 +39,20 @@ public class LlistaAssignatura implements InterficieLista {
     public ArrayList lista() {
         return this.LlistaAssignatura;
     }
-    public Assignatura getObject(int i){
+
+    public Assignatura getObject(int i) {
         return this.LlistaAssignatura.get(i);
     }
+
     //devuelve elemento de una posicion
     @Override
-    public String getElement(int a) {
-        String element =this.LlistaAssignatura.get(a).toString();
+    public String getElement(int asignatura) {
+        String element = this.LlistaAssignatura.get(asignatura).toString();
         return element;
     }
-    
-    public void EliminarElement(int a){
-        this.LlistaAssignatura.remove(a);
+
+    public void EliminarElement(int asignatura) {
+        this.LlistaAssignatura.remove(asignatura);
     }
-    
+
 }
