06c134520ecd311ee260866b6d5729a2862466ee
elenamorey6 <elenamorey23@gmail.com>
2024-11-19 22:33:17 +0100

    no imprime estudiantes ListaEstudiantes.java

--- Practica1/src/practica1/ListaEstudiantes.java
+++ Practica1/src/practica1/ListaEstudiantes.java
@@ -7,15 +7,16 @@ import java.util.ArrayList;
  * @author ellen
  */
 public class ListaEstudiantes {
+
     private ArrayList<Estudiantes> ListaEstudiantes;
 
     public ListaEstudiantes() {
         this.ListaEstudiantes = new ArrayList<>();
     }
-    
+
     public void addEstudiante(Estudiantes alumnoNuevo) {
-        for(Estudiantes e : ListaEstudiantes){
-            if ((e.getNombre().equals(alumnoNuevo.getNombre())) && (e.getDNI().equals(alumnoNuevo.getDNI()))){
+        for (Estudiantes e : ListaEstudiantes) {
+            if ((e.getNombre().equals(alumnoNuevo.getNombre())) && (e.getDNI().equals(alumnoNuevo.getDNI()))) {
                 this.ListaEstudiantes.add(alumnoNuevo);
                 return;  // Si ya existe un estudiante con el mismo nombre y DNI, no lo añadimos
             }
@@ -23,12 +24,18 @@ public class ListaEstudiantes {
         // Si no existe, añadimos al nuevo estudiante
         this.ListaEstudiantes.add(alumnoNuevo);
     }
-    
-    public void imprimirEstudiantes(){
-        for(Estudiantes e : ListaEstudiantes){
-            System.out.println("      ---Nombre: " + e.getNombre() + "       DNI: " + e.getDNI());
+
+    // Imprimir todos los estudiantes
+    public void imprimirEstudiantes() {
+        for (Estudiantes e : ListaEstudiantes) {
+            System.out.println(e);
         }
     }
+
+    public int longitud() {
+        return ListaEstudiantes.size();
+    }
+
     // Método para desmatricular a todos los estudiantes de un curso
     public void desmatricularEstudiantesDeCurso(Curso curso) {
         for (Estudiantes estudiante : ListaEstudiantes) {
