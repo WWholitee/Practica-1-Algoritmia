8a416f5622e72e0cd6b53a897e747e0fbd85f4d8
elenamorey6 <elenamorey23@gmail.com>
2024-11-21 21:50:22 +0100

    Version Final LlistaAssignatura.java

--- Practica1/src/practica1/LlistaAssignatura.java
+++ Practica1/src/practica1/LlistaAssignatura.java
@@ -2,16 +2,19 @@ package practica1;
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Iterator;
 
 /**
  *
  * @author ellen
  */
-public class LlistaAssignatura implements InterficieLista {
+public class LlistaAssignatura implements InterficieLista, Iterable<Assignatura> {
     private ArrayList<Assignatura> LlistaAssignatura;
-    public LlistaAssignatura(){
-        this.LlistaAssignatura= new ArrayList<>();
+
+    public LlistaAssignatura() {
+        this.LlistaAssignatura = new ArrayList<>();
     }
+
     @Override
     public int longitud() {
         return LlistaAssignatura.size();
@@ -26,27 +29,34 @@ public class LlistaAssignatura implements InterficieLista {
     public ArrayList lista() {
         return this.LlistaAssignatura;
     }
+
     @Override
     public String getElement(int a) {
-        String element =this.LlistaAssignatura.get(a).toString();
+        String element = this.LlistaAssignatura.get(a).toString();
         return element;
     }
+
     @Override
-    public Assignatura getObject(int i){
+    public Assignatura getObject(int i) {
         return this.LlistaAssignatura.get(i);
     }
-    
+
     @Override
     public void addObject(Object a) {
         this.LlistaAssignatura.add((Assignatura) a);
     }
-    
-    public void EliminarElement(int a){
+
+    public void EliminarElement(int a) {
         this.LlistaAssignatura.remove(a);
     }
-    
-    public void OrdenarLlista(Object a){
+
+    public void OrdenarLlista(Object a) {
         this.LlistaAssignatura.sort((Comparator<? super Assignatura>) a);
     }
-    
+
+    // Implementación de Iterable<Assignatura>
+    @Override
+    public Iterator<Assignatura> iterator() {
+        return LlistaAssignatura.iterator(); // Usamos el iterador de ArrayList
+    }
 }
